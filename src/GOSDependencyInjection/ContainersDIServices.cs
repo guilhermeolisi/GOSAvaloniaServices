ca38a3a048e3c0673c0c26d02b9c4c7e4397ace7
Guilherme Oliveira <guilhermeolisi@outlook.com>
2025-07-22 22:21:21 -0300

    Backup. Atualizei o Dependency Injection do Sindarin e do BaseLibrary para minha implementação própria para diminiur a dependência do Splat nestas bibliotexas mais básicas e que requerem mais performance.

---
+++ src/GOSDependencyInjection/ContainersDIServices.cs
@@ -0,0 +1,43 @@
+﻿using BaseLibrary.DependencyInjection;
+using Splat;
+
+namespace GOSAvaloniaServices;
+
+public static class ContainersDIServices
+{
+    static IContainer containerGOS = BaseLibrary.DependencyInjection.Locator.ConstanteContainer;
+    static IMutableDependencyResolver containerSplat = Splat.Locator.CurrentMutable;
+    public static void RegisterSingleton<TService, TImplementation>() where TImplementation : TService, new()
+    {
+        //Verifica se já existe um registro para o tipo TService no container do GOSDI se tiver registra também no container do Splat
+        TService? obj = containerGOS.Resolve<TService>();
+        if (obj is null)
+        {
+            obj = new TImplementation();
+            containerSplat.RegisterConstant(obj, typeof(TService));
+        }
+    }
+    public static void RegisterSingleton<TService>(Func<TService> creator)
+    {
+        // Verifica se já existe um registro para o tipo TService no container do GOSDI se tiver registra também no container do Splat
+        TService? obj = containerGOS.Resolve<TService>();
+        if (obj is null)
+        {
+            obj = creator();
+            containerSplat.RegisterConstant(obj, typeof(TService));
+        }
+    }
+    public static void Register<TService>(Func<TService> creator)
+    {
+        // Verifica se já existe um registro para o tipo TService no container do GOSDI se tiver registra também no container do Splat
+        TService? obj = containerGOS.Resolve<TService>();
+        if (obj is null)
+        {
+            containerSplat.Register(() => creator(), typeof(TService));
+        }
+    }
+    public static TService? Resolve<TService>()
+    {
+        return containerGOS.Resolve<TService>() ?? Splat.Locator.Current.GetService<TService>();
+    }
+}
