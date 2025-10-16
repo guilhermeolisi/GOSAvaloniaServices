edd7d5dda837394b77132ab5387f6bc4c3488aa2
Guilherme Oliveira <guilhermeolisi@outlook.com>
2025-10-16 16:25:00 -0300

    Backup. Inclui 2 exemplos no nimloth. publiquei o Dislocation e DislocationPopa

--- src/GOSBaseInsection/CopyTextClipboard.cs
+++ src/GOSBaseInsection/CopyTextClipboard.cs
@@ -1,9 +1,16 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input.Platform;
 
 namespace GOSBaseInjection;
 
 public class CopyTextClipboard : ICopyTextClipboard
 {
+    private SynchronizationContext uiContext;
+    public bool IsOnUIThread => uiContext == SynchronizationContext.Current;
+    public CopyTextClipboard()
+    {
+        uiContext = SynchronizationContext.Current ?? throw new InvalidOperationException("UI thread context is not available.");
+    }
     //https://docs.avaloniaui.net/docs/concepts/services/clipboard
     TopLevel topLevel;
     public void SetTopLevel(object topLevel)
@@ -17,13 +24,46 @@ public class CopyTextClipboard : ICopyTextClipboard
     public async Task<string?> CopyFromClipBoard()
     {
         //return await Application.Current.Clipboard.GetTextAsync();
-        return await topLevel!.Clipboard!.GetTextAsync();
-
+        string? result;
+        if (IsOnUIThread)
+        {
+            result = await topLevel!.Clipboard!.TryGetTextAsync();
+        }
+        else
+        {
+            var tcs = new TaskCompletionSource<string>();
+            uiContext?.Post(async _ =>
+            {
+                try
+                {
+                    var text = await topLevel!.Clipboard!.TryGetTextAsync();
+                    tcs.SetResult(text);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+            }, null);
+            result = await tcs.Task;
+        }
+        return result;
     }
     public async Task CopyToClipBoard(string message)
     {
         if (message is not null)
-            await topLevel!.Clipboard!.SetTextAsync(message);
+        {
+            if (IsOnUIThread)
+            {
+                await topLevel!.Clipboard!.SetTextAsync(message);
+            }
+            else
+            {
+                uiContext?.Post(async _ =>
+                {
+                    await topLevel!.Clipboard!.SetTextAsync(message);
+                }, null);
+            }
+        }
         //await Application.Current.Clipboard.SetTextAsync(message);
     }
     public async Task<bool> IsTextInClipBoard()
